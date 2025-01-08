b73518b96ef8f6c2a569d4ea7a9d302ee6632a36
Matt <matt@shape.tech>
2025-01-08 15:57:12 +0000

    Add navrail with example destinations to main.dart

--- movie_shape/lib/main.dart
+++ movie_shape/lib/main.dart
@@ -10,10 +10,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    int selectedIndex = 0;
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Row(
+          children: [
+            NavigationRail(
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.theaters),
+                  label: Text("Movies"),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.bookmark_outlined),
+                    label: Text("Favourites")),
+                NavigationRailDestination(
+                  icon: Icon(Icons.remove_red_eye),
+                  label: Text("Watch List"),
+                )
+              ],
+              selectedIndex: selectedIndex,
+              backgroundColor: AppConstants.secondaryColour,
+              extended: true,
+            ),
+            Expanded(
+              child: AppBar(
+                title: Text("MovieShape"),
+              ),
+            )
+          ],
         ),
       ),
     );
