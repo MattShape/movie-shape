937e9b831d3bc285a51ea9ddbd16e07638391505
Igor <igor@shape.tech>
2025-01-09 11:52:53 +0000

    Setup repository for OMDb API calls

---
+++ movie_shape/lib/repository/film_repo.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+// Repository for handling API requests with OMDb API
+class FilmRepo {
+  // TODO: search by s request
+
+  // TODO: search by id request
+}
