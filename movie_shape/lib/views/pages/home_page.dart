3fbef6a4f7dc6031a237cd0e2af2cd9e1b8ede92
Matt <matt@shape.tech>
2025-01-10 10:59:55 +0000

    convert homepage to a stateful widget to handle search results

--- movie_shape/lib/views/pages/home_page.dart
+++ movie_shape/lib/views/pages/home_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:movie_shape/helpers/constants.dart';
+import 'package:movie_shape/models/film_summary.dart';
 import 'package:movie_shape/views/widgets/searchbar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<FilmSummary> _filmResults = [];
+
+  void _updateFilmResults(List<FilmSummary> films) {
+    setState(() {
+      _filmResults = films;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
