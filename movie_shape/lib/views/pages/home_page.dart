0f8ad00cad5169bfa3d48379d3daa2965107404f
Matt <matt@shape.tech>
2025-01-10 11:38:39 +0000

    refactor homepage to handle state change from searchbar

--- movie_shape/lib/views/pages/home_page.dart
+++ movie_shape/lib/views/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_shape/helpers/constants.dart';
 import 'package:movie_shape/models/film_summary.dart';
 import 'package:movie_shape/views/widgets/searchbar.dart';
+import 'package:movie_shape/views/widgets/film_list.dart'; // Import the FilmList widget
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,6 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<FilmSummary> _filmResults = [];
 
+  // This function will be called by the search bar to update the list
   void _updateFilmResults(List<FilmSummary> films) {
     setState(() {
       _filmResults = films;
@@ -22,20 +24,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("MovieShape"),
+      ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
-                Text("MovieShape",
-                    style: AppConstants.fontFamily(
-                        color: AppConstants.textColour,
-                        fontSize: AppConstants.companyTitleSize)),
-                Expanded(child: CustomSearchBar()),
+                Expanded(
+                    child:
+                        CustomSearchBar(onSearchResults: _updateFilmResults)),
               ],
             ),
           ),
+          // Film list is now displayed here
+          FilmList(films: _filmResults),
         ],
       ),
     );
