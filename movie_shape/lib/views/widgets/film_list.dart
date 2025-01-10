a21d27aae9bbea17e238b6b38f345116e9786bd9
Matt <matt@shape.tech>
2025-01-10 16:06:38 +0000

    Refactor and styling to film list

--- movie_shape/lib/views/widgets/film_list.dart
+++ movie_shape/lib/views/widgets/film_list.dart
@@ -33,13 +33,27 @@ class _FilmListState extends State<FilmList> {
         itemBuilder: (context, index) {
           final film =
               widget.films[index]; // Access the films using widget.films
-          return ListTile(
-            title: Text(film.title),
-            subtitle: Text(film.year),
-            leading: Image.network(film.poster),
-            onTap: () {
-              _goToDetailPage(film.imdbID);
-            },
+          return Card(
+            color: AppConstants.secondaryFgColour,
+            shadowColor: AppConstants.accentColour,
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            elevation: 4,
+            child: ListTile(
+              title: Text(
+                film.title,
+                style: TextStyle(color: AppConstants.textColour),
+              ),
+              subtitle: Text(
+                film.year,
+                style: TextStyle(color: AppConstants.textColour),
+              ),
+              leading: Image.network(
+                film.poster,
+              ),
+              onTap: () {
+                _goToDetailPage(film.imdbID);
+              },
+            ),
           );
         },
       ),
