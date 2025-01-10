c777034cd106d40067470fd5b8d03ed4be15a54d
Matt <matt@shape.tech>
2025-01-10 13:00:08 +0000

    remove duplicate import

--- movie_shape/lib/repository/film_repo.dart
+++ movie_shape/lib/repository/film_repo.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:movie_shape/models/film.dart';
 
 import "../models/film_search.dart";
@@ -13,14 +12,15 @@ class FilmRepo {
 
   static Future<Film> fetchFilmById(String id) async {
     final response = await http
-      .get(Uri.parse('https://www.omdbapi.com/?apikey=810d5ee8&i=${id}'));
-    
+        .get(Uri.parse('https://www.omdbapi.com/?apikey=810d5ee8&i=${id}'));
+
     if (response.statusCode == 200) {
       return Film.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
     } else {
       throw Exception('Failed to load film');
     }
   }
+
   static Future<FilmsSearch?> fetchFilmByTitle(String title) async {
     final response = await http
         .get(Uri.parse("$_baseUrl?apikey=$_apiKey&type=movie&s=$title"));
