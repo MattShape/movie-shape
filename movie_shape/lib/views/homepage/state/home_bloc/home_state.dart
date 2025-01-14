a171556dfa0b08ac5f5e00dc517fc7de05cda823
Matt <matt@shape.tech>
2025-01-14 16:01:40 +0000

    define all states for homepage
    set up definitions for homepage states: initial, loading, loaded and error.

--- movie_shape/lib/views/homepage/state/home_bloc/home_state.dart
+++ movie_shape/lib/views/homepage/state/home_bloc/home_state.dart
@@ -1,6 +1,19 @@
 part of 'home_bloc.dart';
 
-@immutable
-sealed class HomeState {}
+abstract class HomeState {
+  const HomeState();
+}
 
-final class HomeInitial extends HomeState {}
+class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeLoaded extends HomeState {
+  final List<FilmSummary> filmlist;
+  const HomeLoaded(this.filmlist);
+}
+
+class HomeError extends HomeState {
+  final String message;
+  const HomeError(this.message);
+}
