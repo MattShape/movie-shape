e3640d4e260e00c5785de543729e9689b706607c
Matt <matt@shape.tech>
2025-01-14 16:28:39 +0000

    fix typo

--- movie_shape/lib/repository/film_repo_implemented.dart
+++ movie_shape/lib/repository/film_repo_implemented.dart
@@ -42,7 +42,7 @@ class FilmRepoImplemented implements FilmRepo {
         throw Exception("Fail");
       }
     } catch (e) {
-      throw Exception("failed to load film list: ${e.toString()}")
+      throw Exception("failed to load film list: ${e.toString()}");
     }
   }
 
