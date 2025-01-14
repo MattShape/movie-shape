6a7936c0b23efd8b5980a6ddf3eb7f47278f43ca
Matt <matt@shape.tech>
2025-01-14 16:25:10 +0000

    remove optional null from film list API call

--- movie_shape/lib/repository/film_repo.dart
+++ movie_shape/lib/repository/film_repo.dart
@@ -3,7 +3,7 @@ import 'package:movie_shape/models/film_summary.dart';
 
 abstract class FilmRepo {
   Future<Film> getFilmById({required String id});
-  Future<List<FilmSummary>?> searchFilmsByTitle({required String searchQuery});
+  Future<List<FilmSummary>> searchFilmsByTitle({required String searchQuery});
   Future<List<FilmSummary>?> getFavouritedFilms();
   Future<List<FilmSummary>?> getWatchlistFilms();
   Future<void> addFilmToWatchlist({required FilmSummary film});
