2a79396bb82eba8c355d16b9486a5d0d3d951d35
Igor <igor@shape.tech>
2025-01-14 16:57:15 +0000

    Add bloc for favourites button
    Add events for favourites button bloc
    Add state for favourites button bloc

---
+++ movie_shape/lib/reusable/favourite_button/bloc/favourite_button_bloc.dart
@@ -0,0 +1,65 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import 'package:movie_shape/models/film_summary.dart';
+import 'package:movie_shape/repository/film_repo_implemented.dart';
+
+part 'favourite_button_event.dart';
+part 'favourite_button_state.dart';
+
+class FavouriteButtonBloc extends Bloc<FavouriteButtonEvent, FavouriteButtonState> {
+  FavouriteButtonBloc({required this.filmId}) : super(FavouriteButtonInitial()) {
+    on<FavouriteButtonDisplayed>(_onFavouriteButtonDisplayed);
+    on<FavouriteButtonPressed>(_onFavouriteButtonPressed);
+  }
+
+  final String filmId;
+
+  // handle logic when favourite button has displayed
+  void _onFavouriteButtonDisplayed(event, emit) async {
+    emit(FavouriteButtonLoading());
+
+    try {
+      List<FilmSummary>? favourites =
+          await FilmRepoImplemented().getFavouritedFilms();
+      if (favourites != null) {
+        // update state that film is loaded (true)
+        emit(FavouriteButtonLoaded(
+        // checks if one element matches the condition
+        favourites.any((film) => film.imdbID == filmId)));
+      } else {
+        emit(FavouriteButtonLoaded(false));
+      }
+    } catch (e) {
+      emit(FavouriteButtonError(e.toString()));
+    }
+  }
+
+  void _onFavouriteButtonPressed(event, emit) async {
+    emit(FavouriteButtonLoading());
+
+    try {
+      bool isInFavouritesList = false;
+
+      List<FilmSummary>? favouritesList = 
+          await FilmRepoImplemented().getFavouritedFilms();
+
+      if (favouritesList != null) {
+        isInFavouritesList = 
+            favouritesList.any((film) => film.imdbID == event.film.imdbID);
+      }
+      
+      if (isInFavouritesList) {
+        await FilmRepoImplemented().removeFilmFromFavourites(film: event.film);
+
+        emit(FavouriteButtonLoaded(false));
+      } else {
+        await FilmRepoImplemented().addFilmToFavourites(film: event.film);
+
+        emit(FavouriteButtonLoaded(true));
+      }
+    } catch (e) {
+      emit(FavouriteButtonError(e.toString()));
+    }
+  }
+}
