2b82df6ba4d562df689f4f2bf4ce86d47b974370
Matt <matt@shape.tech>
2025-01-15 13:13:25 +0000

    refactor to remove statefulness
    convert to stateless widget, remove unnecessary state, variables, logic and methods to allow BLoC to handle logic.

--- movie_shape/lib/views/widgets/searchbar.dart
+++ movie_shape/lib/views/widgets/searchbar.dart
@@ -1,36 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:movie_shape/models/film_search.dart';
-import 'package:movie_shape/models/film_summary.dart';
-import 'package:movie_shape/repository/film_repo_implemented.dart';
 
-class CustomSearchBar extends StatefulWidget {
-  final Function(List<FilmSummary>) onSearchResults;
+class CustomSearchBar extends StatelessWidget {
+  final Function(String) onSearchResults;
 
-  const CustomSearchBar({super.key, required this.onSearchResults});
+  CustomSearchBar({super.key, required this.onSearchResults});
 
-  @override
-  State<CustomSearchBar> createState() => _CustomSearchBarState();
-}
-
-class _CustomSearchBarState extends State<CustomSearchBar> {
   final TextEditingController _controller = TextEditingController();
-  String _query = "";
 
-  Future<void> _handleSearch(String title) async {
-    final List<FilmSummary>? response =
-        await FilmRepoImplemented().searchFilmsByTitle(searchQuery: title);
-    // print(response?.films[0].title);
-    if (response != null) {
-      widget.onSearchResults(response);
-    } else {
-      widget.onSearchResults([]);
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void _handleSearch(String title) {
+    onSearchResults(title);
   }
 
   @override
@@ -38,17 +16,8 @@ class _CustomSearchBarState extends State<CustomSearchBar> {
     return SearchBar(
       hintText: "Search for a film...",
       controller: _controller,
-      onChanged: (value) {
-        setState(() {
-          _query = value;
-        });
-      },
       onSubmitted: (value) {
         _handleSearch(value);
-        setState(() {
-          _query = "";
-        });
-        _controller.clear();
       },
       leading: Icon(Icons.search),
     );
