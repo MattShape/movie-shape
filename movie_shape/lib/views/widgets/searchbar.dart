88825d1b90b37cc54dacb96847dedc3363779c45
Matt <matt@shape.tech>
2025-01-15 19:53:59 +0000

    add function to clear textfield when clear results function is passed in

--- movie_shape/lib/views/widgets/searchbar.dart
+++ movie_shape/lib/views/widgets/searchbar.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 class CustomSearchBar extends StatelessWidget {
   final Function(String) onSearchResults;
+  final VoidCallback? onClear;
 
-  CustomSearchBar({super.key, required this.onSearchResults});
+  CustomSearchBar({super.key, required this.onSearchResults, this.onClear});
 
   final TextEditingController _controller = TextEditingController();
 
   void _handleSearch(String title) {
     onSearchResults(title);
+  }
+
+  void _handleClear() {
+    _controller.clear();
+    if (onClear != null) {
+      onClear!();
+    }
   }
 
   @override
@@ -20,6 +28,9 @@ class CustomSearchBar extends StatelessWidget {
         _handleSearch(value);
       },
       leading: Icon(Icons.search),
+      trailing: onClear != null
+          ? [IconButton(onPressed: _handleClear, icon: Icon(Icons.clear))]
+          : null,
     );
   }
 }
