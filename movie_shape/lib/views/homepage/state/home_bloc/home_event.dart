176725acd753e44d5205b7e2bba49a6f2e860a3a
Matt <matt@shape.tech>
2025-01-15 13:18:32 +0000

    Add OnSearchSubmit event to allow searchbar to utilise BLoC

--- movie_shape/lib/views/homepage/state/home_bloc/home_event.dart
+++ movie_shape/lib/views/homepage/state/home_bloc/home_event.dart
@@ -1,4 +1,10 @@
 part of 'home_bloc.dart';
 
-@immutable
-sealed class HomeEvent {}
+abstract class HomeEvent {
+  const HomeEvent();
+}
+
+class OnSearchSubmit extends HomeEvent {
+  final String searchQuery;
+  OnSearchSubmit(this.searchQuery);
+}
