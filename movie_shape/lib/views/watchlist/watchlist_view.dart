a0c0dcc13e318582303509ea3885ae1823663665
Matt <matt@shape.tech>
2025-01-15 13:16:02 +0000

    Add searchbar to watchlist to allow searching

--- movie_shape/lib/views/watchlist/watchlist_view.dart
+++ movie_shape/lib/views/watchlist/watchlist_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_shape/helpers/constants.dart';
 import 'package:movie_shape/models/film.dart';
 import 'package:movie_shape/repository/film_repo_implemented.dart';
 import 'package:movie_shape/reusable/list/list_card.dart';
 import 'package:movie_shape/views/pages/film_detail_page.dart';
 import 'package:movie_shape/views/watchlist/state/watchlist_bloc/watchlist_bloc.dart';
+import 'package:movie_shape/views/widgets/searchbar.dart';
 
 class WatchlistView extends StatelessWidget {
   const WatchlistView({super.key});
@@ -22,42 +22,51 @@ class WatchlistView extends StatelessWidget {
       );
     }
 
-    return BlocBuilder<WatchlistBloc, WatchlistState>(
-      builder: (context, state) {
-        if (state is WatchlistLoading) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+    return Column(
+      children: [
+        CustomSearchBar(onSearchResults: (searchQuery) {
+          context.read<WatchlistBloc>().add(SearchWatchlist(searchQuery));
+        }),
+        Expanded(
+          child: BlocBuilder<WatchlistBloc, WatchlistState>(
+            builder: (context, state) {
+              if (state is WatchlistLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-        if (state is WatchlistLoaded) {
-          if (state.watchlist.isEmpty) {
-            return const Center(
-              child: Text('Your watchlist is empty'),
-            );
-          }
+              if (state is WatchlistLoaded) {
+                if (state.watchlist.isEmpty) {
+                  return const Center(
+                    child: Text('Your watchlist is empty'),
+                  );
+                }
 
-          return ListView.builder(
-            itemCount: state.watchlist.length,
-            itemBuilder: (context, index) {
-              final film = state.watchlist[index];
-              return ListCard(
-                film: film,
-                onTap: (val) {
-                  _goToDetailPage(val);
-                },
-              );
+                return ListView.builder(
+                  itemCount: state.watchlist.length,
+                  itemBuilder: (context, index) {
+                    final film = state.watchlist[index];
+                    return ListCard(
+                      film: film,
+                      onTap: (val) {
+                        _goToDetailPage(val);
+                      },
+                    );
+                  },
+                );
+              }
+
+              if (state is WatchlistError) {
+                return Center(
+                  child: Text(state.message),
+                );
+              }
+              return Container();
             },
-          );
-        }
-
-        if (state is WatchlistError) {
-          return Center(
-            child: Text(state.message),
-          );
-        }
-        return Container();
-      },
+          ),
+        ),
+      ],
     );
   }
 }
