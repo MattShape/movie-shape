c1e3ffa979e25c53351a17a08139c98f22e9c54a
Matt <matt@shape.tech>
2025-01-17 12:08:42 +0000

    Add favourite and watchlist buttons to card

--- movie_shape/lib/reusable/list/list_card.dart
+++ movie_shape/lib/reusable/list/list_card.dart
@@ -4,6 +4,8 @@ import 'package:movie_shape/helpers/constants.dart';
 import 'package:movie_shape/models/film.dart';
 import 'package:movie_shape/models/film_summary.dart';
 import 'package:movie_shape/repository/film_repo_implemented.dart';
+import 'package:movie_shape/reusable/favourite_button/favourite_button.dart';
+import 'package:movie_shape/reusable/watchlist_button/watchlist_button.dart';
 import 'package:movie_shape/views/pages/film_detail_page.dart';
 import 'package:movie_shape/views/watchlist/state/watchlist_bloc/watchlist_bloc.dart';
 
@@ -17,62 +19,74 @@ class ListCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 160,
-      child: Card(
-        color: AppConstants.primaryColour,
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 13.0),
-        // child: ListTile(
-        //   onTap: () {
-        //     onTap(film.imdbID);
-        //   },
-        // ),
-
-        child: Row(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(65.0),
+        child: Card(
+          color: AppConstants.primaryColour,
+          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 13.0),
+          child: InkWell(
+            onTap: () {
+              onTap(film.imdbID);
+            },
+            child: Row(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(6.0),
-                    child: Image.network(
-                      film.poster,
-                      height: 120,
-                      fit: BoxFit.cover,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6.0),
+                        child: Image.network(
+                          film.poster,
+                          height: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 22.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          film.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: AppConstants.textColour,
+                              fontSize: 24),
+                        ),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          "Release date: ${film.year}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              color: AppConstants.textColour,
+                              fontSize: 10),
+                        ),
+                      ],
                     ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FavouriteButton(film: film),
+                      WatchlistButton(film: film)
+                    ],
                   ),
                 )
               ],
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 22.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      film.title,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: AppConstants.textColour,
-                          fontSize: 24),
-                    ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Text(
-                      "Release date: ${film.year}",
-                      style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: AppConstants.textColour,
-                          fontSize: 10),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
