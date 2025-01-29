da887d5eec1e6c9aeb5d7525dd7ab2e4b6d69673
Matt <matt@shape.tech>
2025-01-29 12:30:31 +0000

    feat: plot button

---
+++ movie_shape/lib/reusable/plot_button/plot_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlotButton extends StatelessWidget {
+  const PlotButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
