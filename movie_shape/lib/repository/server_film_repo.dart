6bd75c00c59e2637d226ec7cd105e40f09b8afaf
Igor <igor@shape.tech>
2025-01-31 09:43:15 +0000

    Refactor: Decouple repo implementation from FIlm
    Move fromServerJson from Film class to the server film repo

--- movie_shape/lib/repository/server_film_repo.dart
+++ movie_shape/lib/repository/server_film_repo.dart
@@ -2,8 +2,76 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movie_shape/models/film.dart';
 import 'package:movie_shape/models/film_summary.dart';
+import 'package:movie_shape/models/rating.dart';
 import 'package:movie_shape/repository/film_repo.dart';
 import "../models/film_search.dart";
+
+Film fromServerJson(Map<String, dynamic> json) {
+  const String defaultPosterUrl =
+      'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg';
+
+  var film = Film(
+    title: json['title'] as String? ?? 'N/A',
+    year: (json['release_date'] as int?).toString() ?? 'N/A',
+    rated: json['certificate'] as String? ?? 'N/A',
+    released: json['released'] as String? ?? 'N/A',
+    runtime: json['runtime'] as String? ?? 'N/A',
+    genre: json['genre'] as String? ?? 'N/A',
+    director: json['director'] as String? ?? 'N/A',
+    // TODO: NO WRITER FROM SERVER RESPONSE
+    writer: (json['writer'] as String?)
+            ?.split(',')
+            .map((writer) => writer.trim())
+            .toList() ??
+        ['N/A'],
+    actors: [
+      json['star1'] as String? ?? 'N/A',
+      json['star2'] as String? ?? 'N/A',
+      json['star3'] as String? ?? 'N/A',
+      json['star4'] as String? ?? 'N/A',
+    ],
+    plot: json['description'] as String? ?? 'N/A',
+    // TODO: NO LANGUAGE FROM SERVER RESPONSE
+    language: (json['language'] as String?)
+            ?.split(',')
+            .map((actor) => actor.trim())
+            .toList() ??
+        ['N/A'],
+    // TODO: NO COUNTRY FROM SERVER RESPONSE
+    country: json['country'] as String? ?? 'N/A',
+    // TODO: NO AWARDS FROM SERVER RESPONSE
+    awards: (json['awards'] as String?)
+            ?.split(RegExp(r'[.&]')) // Split by "." and "&"
+            .map((part) => part.trim()) // Trim whitespace
+            .toList() ??
+        [],
+    poster: Uri.tryParse(json['poster_link'] ?? '')?.hasAbsolutePath == true
+        ? json['poster_link']
+        : defaultPosterUrl,
+    ratings: [
+      Rating(
+          source: 'IMDb',
+          value: (json['rating'] as double?).toString() ?? 'N/A'),
+      Rating(
+          source: 'Metascore',
+          value: (json['meta_score'] as int?).toString() ??
+              'N/A'), // SQL Column: meta_score
+    ],
+    metascore: (json['meta_score'] as int?).toString() ?? 'N/A',
+    imdbRating: (json['rating'] as double?).toString() ?? 'N/A',
+    imdbVotes: (json['no_of_votes'] as int?).toString() ??
+        'N/A', // SQL Column: no_of_votes
+    imdbId: (json['id'] as int?).toString() ?? 'N/A',
+    type: 'Film',
+    dvd: 'N/A',
+    boxOffice: json['gross'] as String? ?? 'N/A',
+    // TODO: no production from SERVER RESPONSE
+    production: json['production'] as String? ?? 'N/A',
+    response: 'True',
+  );
+
+  return film;
+}
 
 class ServerFilmRepo implements FilmRepo {
   static const String _baseUrl = "http://localhost:8000/api";
@@ -14,7 +82,7 @@ class ServerFilmRepo implements FilmRepo {
       String query = "${_baseUrl}/movies/${id}";
       final response = await http.get(Uri.parse(query));
       if (response.statusCode == 200) {
-        return Film.fromServerJson(
+        return fromServerJson(
             jsonDecode(response.body) as Map<String, dynamic>);
       } else {
         throw Exception('Failed to load film');
@@ -109,8 +177,7 @@ class ServerFilmRepo implements FilmRepo {
         List<dynamic> filmsJson = jsonDecode(response.body);
         // Map the list of films into Film objects using Film.fromJson() and convert it to a List<Film>
         final films = filmsJson
-            .map((filmJson) =>
-                Film.fromServerJson(filmJson as Map<String, dynamic>))
+            .map((filmJson) => fromServerJson(filmJson as Map<String, dynamic>))
             .toList();
         return films.map((film) => FilmSummary.fromFilm(film)).toList();
       } else {
